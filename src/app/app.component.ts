42d93a9e69edea2834b354b006621ff1f4013434
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2025-02-14 23:33:17 +0100

    FE-2 Generate general components; Define css variables; Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'miars-frontend';
-}
+export class AppComponent {}
