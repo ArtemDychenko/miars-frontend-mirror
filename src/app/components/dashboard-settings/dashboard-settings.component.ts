3f187e1d1f034a4f358a2d8f70486429fdeee628
Artem Dychenko <s192441@student.pg.edu.pl>
2025-03-22 22:11:32 +0100

    feat: add slide-toggle component, fix color in dashboard-settings styles

--- src/app/components/dashboard-settings/dashboard-settings.component.ts
+++ src/app/components/dashboard-settings/dashboard-settings.component.ts
@@ -2,8 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { FormsModule } from '@angular/forms';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from '../button/button.component';
+import { SlideToggleComponent } from '../slide-toggle/slide-toggle.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -11,8 +12,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatDialogModule,
     ToggleSwitchModule,
     FormsModule,
-    MatSlideToggle,
     MatButtonModule,
+    ButtonComponent,
+    SlideToggleComponent,
   ],
   selector: 'app-dashboard-settings',
   styleUrl: './dashboard-settings.component.scss',
---
+++ src/app/components/slide-toggle/slide-toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-slide-toggle',
+  imports: [MatSlideToggle],
+  templateUrl: './slide-toggle.component.html',
+  styleUrl: './slide-toggle.component.scss',
+})
+export class SlideToggleComponent {}
